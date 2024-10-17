3e0424f5d63585b918444203166e9d2bfa86fa76
Axel Brisse <axel.brisse@gmail.com>
2024-10-18 01:45:31 +0200

    newlines

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -104,7 +104,7 @@ impl Command {
 }
 
 #[derive(Debug, Serialize, Deserialize, Eq, PartialEq, Hash, Clone)]
-enum Resource {
+pub enum Resource {
     Linemate,
     Deraumere,
     Sibur,
@@ -115,15 +115,16 @@ enum Resource {
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-struct Egg {
-    team_name: String,
-    start_frame: u64,
+pub struct Egg {
+    pub team_name: String,
+    pub start_frame: u64,
 }
+
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Cell {
-    players: Vec<String>,
-    resources: HashMap<Resource, usize>,
-    eggs: Vec<Egg>,
+    pub players: Vec<String>,
+    pub resources: HashMap<Resource, usize>,
+    pub eggs: Vec<Egg>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -142,11 +143,13 @@ impl Cell {
         }
     }
 }
+
 impl Map {
     pub fn new(width: usize, height: usize) -> Self {
         let map = vec![vec![Cell::new(); width]; height];
         Self { map, width, height }
     }
 }
+
 pub const GFX_PORT: u16 = 4343;
 pub const MAX_COMMANDS: usize = 10;
