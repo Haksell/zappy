152062ab875324a76213f59424d417267ef5a3ee
Axel Brisse <axel.brisse@gmail.com>
2024-11-15 00:05:48 +0100

    .

--- gfx/src/engines/torus/mod.rs
+++ gfx/src/engines/torus/mod.rs
@@ -147,10 +147,13 @@ fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
     torus_transform: Res<TorusTransform>,
 ) {
+    // TODO: Full radius is 1.0 + minor_radius. Camera distance from origin should be based on that.
+    // Take inspiration from this commit for camera bundle usage:
+    // https://github.com/Haksell/zappy/commit/423b6ccff892d1b3dff3f48058f5eb85f5d56521
     commands.spawn(Camera3dBundle {
         transform: Transform::from_xyz(0.0, 0.0, 4.2).looking_at(Vec3::ZERO, Vec3::Y),
         ..Default::default()
-    }); // TODO: depends on minor rotation
+    });
 
     commands.spawn(PointLightBundle {
         point_light: PointLight {
