2b9c3112cf3446de345664abf44a7cd6cd578cc6
Axel Brisse <axel.brisse@gmail.com>
2024-11-15 18:04:56 +0100

    .

--- gfx/src/engines/torus/mod.rs
+++ gfx/src/engines/torus/mod.rs
@@ -2,7 +2,6 @@
 // TODO: share most of the code with 2D bevy Renderer
 // TODO: button to swap main axis (probably a bad idea)
 // TODO: button to switch from 2D to torus and vice-versa?
-// TODO: torus x and y rotation with WASD
 // TODO: ESPAAAAAAAAAACE
 // TODO: optimize mesh (right now every corner appears 4 times) (mabe unimportant for reasons)
 
