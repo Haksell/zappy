8b30022cbb7fda02c3a5cf18b05fe8e935bcacb9
solvictor <victor.egret.pro@gmail.com>
2024-11-16 00:55:10 +0100

    Escape gfx exit

--- gfx/src/engines/console.rs
+++ gfx/src/engines/console.rs
@@ -207,7 +207,7 @@ pub async fn render(
 
         tokio::select! {
             Some(key) = event_rx.recv() => {
-                if key.code == KeyCode::Char('q') {
+                if key.code == KeyCode::Esc {
                     break;
                 }
             }
