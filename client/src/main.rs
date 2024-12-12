1252ba7bbfa2bdf44c5e66dffb7ebc4256302cd3
solvictor <victor.egret.pro@gmail.com>
2024-12-12 03:27:55 +0100

    Client skeleton

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,4 +1,8 @@
 use clap::Parser;
+use std::{
+    io::{Read as _, Write},
+    net::TcpStream,
+};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -6,40 +10,34 @@ struct Args {
     #[arg(short('n'), long, help = "Team name")]
     team: String,
 
-    #[arg(short, long, help = "Port number")]
+    #[arg(short, long, default_value_t = 8080, help = "Port of the server.")]
     port: u16,
 
-    #[arg(long, default_value = "localhost")]
-    host: String,
+    #[arg(short, long, default_value_t = String::from("127.0.0.1"), help = "Address of the server.")]
+    address: String,
 }
 
-use rustyline::error::ReadlineError;
-use rustyline::{DefaultEditor, Result};
-
-fn main() -> Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
-    println!("{:?}", args);
-    let mut rl = DefaultEditor::new()?;
-    loop {
-        let readline = rl.readline(">> ");
-        match readline {
-            Ok(line) => {
-                let _ = rl.add_history_entry(line.as_str()); // TODO: handle?
-                println!("Line: {}", line);
-            }
-            Err(ReadlineError::Interrupted) => {
-                println!("CTRL-C");
-                break;
-            }
-            Err(ReadlineError::Eof) => {
-                println!("CTRL-D");
-                break;
-            }
-            Err(err) => {
-                println!("Error: {:?}", err);
-                break;
-            }
-        }
+
+    let mut stream = TcpStream::connect(format!("{}:{}", args.address, args.port))?;
+    eprintln!("Connected to server");
+
+    let mut buffer = [0; 1024];
+    let bytes_read = stream.read(&mut buffer).unwrap();
+    let response = String::from_utf8_lossy(&buffer[..bytes_read]);
+    println!("Server response: {}", response);
+    if response != "BIENVENUE\n" {
+        return Err("Server did not greet (Maybe server is not a zappy server)".into());
     }
+
+    // TODO validate team
+    stream.write_all(args.team.as_bytes())?;
+    let bytes_read = stream.read(&mut buffer).unwrap();
+    let response = String::from_utf8_lossy(&buffer[..bytes_read]);
+    println!("Server response: {}", response);
+
+    // TODO ai
+
     Ok(())
 }
