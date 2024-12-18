ca3e9712d0079dedb83420f27478084c8dd3c3eb
solvictor <victor.egret.pro@gmail.com>
2024-12-18 19:20:42 +0100

    Command sending

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use shared::HANDSHAKE_MSG;
 use std::{
-    io::{BufRead as _, BufReader, Write},
+    io::{BufRead as _, BufReader, Lines, Write},
     net::TcpStream,
 };
 
@@ -16,6 +16,19 @@ struct Args {
 
     #[arg(short, long, default_value_t = String::from("127.0.0.1"), help = "Address of the server.")]
     address: String,
+}
+
+fn send_command(
+    sender: &mut TcpStream,
+    receiver: &mut Lines<BufReader<TcpStream>>,
+    command: &str,
+) -> Result<String, Box<dyn std::error::Error>> {
+    sender.write(command.as_bytes())?;
+    sender.flush()?;
+    receiver
+        .next()
+        .ok_or_else(|| "No response from server".into())
+        .and_then(|response| response.map_err(|e| e.into()))
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -48,22 +61,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     if dimensions.len() != 2 {
         return Err("Invalid dimensions from server".into());
     }
-    let x: usize = dimensions[0].parse()?;
-    let y: usize = dimensions[1].parse()?;
+
+    let width: usize = dimensions[0].parse()?;
+    let height: usize = dimensions[1].parse()?;
 
     if n_clients == 0 {
         return Err(format!("The team '{}' is full.", args.team).into());
     }
 
-    // stream.write("".as_bytes());
-
-    while let Some(Ok(line)) = lines.next() {
-        println!("Received line '{}'", line);
+    loop {
+        eprintln!("{}", send_command(&mut sender, &mut lines, "move")?);
+        eprintln!("{}", send_command(&mut sender, &mut lines, "right")?);
     }
-    eprintln!("Connection lost.");
+
+    // eprintln!("Connection lost.");
 
     // TODO validate team
     // TODO ai
 
-    Ok(())
+    // Ok(())
 }
